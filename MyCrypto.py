3746c8bb7ee35efa9e890689a4e4ce1c5bcfb0af
romano <romano@alunos.utfpr.edu.br>
2017-10-05 22:18:26 -0300

    hash password in sha224

--- MyCrypto.py
+++ MyCrypto.py
@@ -9,6 +9,11 @@ from Crypto import Random
 
 class MyCrypto():
 
+    # hash password in SHA224
+    def hash_password_web(self, password):
+        hash_pass = hashlib.sha224(password).hexdigest()
+        return hash_pass
+
     # hash password in SHA256
     def hash_password(self, password):
         hash_pass = hashlib.sha256(password).hexdigest()
@@ -19,14 +24,14 @@ class MyCrypto():
         return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
 
     def encrypt(self, message, key):
-        key = self.hash_password(key)[:16]
+        key = self.hash_password_web(key)[:16]
         message = self.pad(message)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(key, AES.MODE_CBC, iv)
         return iv + cipher.encrypt(message)
 
     def decrypt(self, ciphertext, key):
-        key = self.hash_password(key)[:16]
+        key = self.hash_password_web(key)[:16]
         iv = self.pad(ciphertext)[:AES.block_size]
         cipher = AES.new(key, AES.MODE_CBC, iv)
         plaintext = cipher.decrypt(ciphertext[AES.block_size:])
